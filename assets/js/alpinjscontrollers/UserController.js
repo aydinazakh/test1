a520592d68da3af334d0894ee911c1a2da697933
aydinazakh <azakhaydin@gmail.com>
2025-05-22 00:32:42 +0330

    part #19

--- assets/js/alpinjscontrollers/UserController.js
+++ assets/js/alpinjscontrollers/UserController.js
@@ -2,9 +2,12 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('usersData' , function(){
         return {
             users: [],
+            isLoading: false,
             gitusers(){
+                this.isLoading - true
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                     this.users = res.data
+                    this.isloading - false
                     console.log(res);
                 })
             }
